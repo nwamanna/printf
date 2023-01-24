4ca82051ef8bc8f4cf77d6f8b7325302012b752a
Nwamanna <nwamannazika@gmail.com>
2023-01-24 08:15:06 +0100

    main

--- main.c
+++ main.c
@@ -11,11 +11,11 @@ int main(void)
 	int count_hmd = 0;
 
 	write(1, "STANDARD:\n", 10);
-	count_std = printf("My name starts with: %d new\n", 56794);
-	printf("I printed %d characters\n", count_std);
+	count_std = printf("String:[%s]\n", "I am a string !");
+	printf("i printed %d characters", count_std);
 	write(1, "_________________________\n", 26);
 	write(1, "HOME_MADE:\n", 11);
-	count_hmd = _printf("My name starts with: %d new\n", 56794);
-	printf("I printed %d characters\n", count_hmd);
+	count_hmd = _printf("String:[%s]\n", "I am a string !");
+	printf("i printed %d characters", count_hmd);
 	return (0);
 }
